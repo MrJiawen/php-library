fd416ee08a1ca3184f7b631bc71da77e770d2ac3
chenjiawen <604989282@qq.com>
2017-05-18 12:29:18 +0200

    mr.wen 添加uuid和验证码

--- Src/Tool/CommonTool.php
+++ Src/Tool/CommonTool.php
@@ -2,15 +2,21 @@
 
 namespace CjwPhpLibary\Src\Tool;
 
+use Gregwar\Captcha\CaptchaBuilder;
+use Gregwar\Captcha\PhraseBuilder;
+use Ramsey\Uuid\Uuid;
+
 /** 通用的工具类
  * Class CommonTool
  * @package CjwPhpLibary\Src\Tool
  */
 class CommonTool
 {
+    protected $identifyingCode = 'identifying_code';
+
     public function __construct()
     {
-       
+
     }
 
     /** 验证码的生成器
@@ -18,31 +24,55 @@ class CommonTool
      * @param int $height
      * @param null $filename
      */
-    public function yzmGenerator($width = 100 , $height = 40, $filename = null){
-        $phrase= new PhraseBuilder;
-        $code=$phrase->build(4);
+    public function identifyingCodeGenerator($width = 100, $height = 40, $filename = null)
+    {
+        $phrase = new PhraseBuilder();
+        $code = $phrase->build(4);
         //生成验证码图片的Builder对象，配置相应属性
-        $builder = new CaptchaBuilder($code,$phrase);
+        $builder = new CaptchaBuilder($code, $phrase);
         //设置背景颜色
-        $builder->setBackgroundColor(220,220,220);
+        $builder->setBackgroundColor(220, 220, 220);
         $builder->setMaxAngle(25);
         $builder->setMaxBehindLines(0);
         $builder->setMaxFrontLines(0);
         //可以设置图片宽高及字体
-        $builder->build($width , $height, null);
+        $builder->build($width, $height, null);
         // $builder->build($width = 100, $height = 40, $font = null);
         //获取验证码的内容
         $phrase = $builder->getPhrase();
 
         //把内容存入session
-        \Session::flash($this->yzmName, $phrase);
+        \Session::flash($this->identifyingCode, $phrase);
         //生成图片
         header("Cache-Control: no-cache, must-revalidate");
         header("Content-Type:image/jpeg");
-        if(empty($filename)){
+        if (empty($filename)) {
             $builder->output();
-        }else{
+        } else {
             $builder->save($filename);
         }
     }
+
+    /** 验证码的验证器
+     * @param $identifyingCode
+     * @return bool
+     */
+    public function identifyingCodeVerification($identifyingCode)
+    {
+        $sysIdentifying = \Session::get($this->identifyingCode);
+        if ($identifyingCode == null) return false;
+        return $identifyingCode == $sysIdentifying ? true : false;
+    }
+
+    /** 生成uuid
+     * @return mixed
+     */
+    public function getUuid()
+    {
+        $temp = Uuid::uuid4();
+        $uuid = $temp->getHex();#uuid
+        return $uuid;
+    }
+
+
 }
