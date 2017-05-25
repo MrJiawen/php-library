439d4d40293ee0b8ce5eb8111df153f4b2513517
chenjiawen <604989282@qq.com>
2017-05-25 21:58:06 +0800

    mr.wen 添加ip查询的服务类

---
+++ Src/Service/IpAddress.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CjwPhpLibary\Src\Service;
+
+use CjwPhpLibary\Src\Tool\CurlRequest;
+
+/** ip地址查询类别
+ * Class IpAddress
+ * @package CjwPhpLibary\Src\Service
+ */
+class IpAddress
+{
+
+    /**ip的归属地查询
+     * @param $ip
+     * @return mixed
+     * @throws \Psy\Exception\ErrorException
+     */
+    public function ipAddressQuery($ip)
+    {
+        if (empty($ip))
+            simpleError('please set CjwPhpLibary\Src\Tool\Common::ipAddressQuery($ip) param', __FILE__, __LINE__);
+
+        $curl = new CurlRequest();
+        $url = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php';
+        $data = [
+            'format' => 'json',
+            'ip' => $ip
+        ];
+        $result = $curl->CurlRequest($url, $data, 'get');
+
+        $result_data = json_decode($result['data'], true);
+
+        return $result_data;
+    }
+
+    /**查询本机的IP地址
+     * @return mixed
+     */
+    public function myIpAddress()
+    {
+
+        $curl = new CurlRequest();
+        $url = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php';
+        $data = [
+            'format' => 'json'
+        ];
+        $result = $curl->CurlRequest($url, $data, 'get');
+
+        $result_data = json_decode($result['data'], true);
+
+        return $result_data;
+    }
+}
