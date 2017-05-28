375190423aa31141942e2a5fa17d1d543b84e6f1
chenjiawen <604989282@qq.com>
2017-05-29 00:43:47 +0800

    mr.wen 封装对jsonString操作的全局函数

--- Src/Function/Array.php
+++ Src/Function/Array.php
@@ -35,5 +35,56 @@ function array_keys_exists($value, $array)
  */
 function toArray($obj)
 {
-    return json_decode(json_encode($obj),true);
+    return json_decode(json_encode($obj), true);
+}
+
+
+/** 对json序列化的字符串进行添加或修改某个值
+ * @param $key
+ * @param $value
+ * @param $obj
+ * @return string
+ */
+function jsonString_add($key, $value, $obj)
+{
+    $obj = json_decode($obj, true);
+    $operate = &$obj;
+    $key = explode('.', $key);
+
+    for ($i = 0; $i < count($key); $i++) {
+        if (isset($operate[$key[$i]]) && is_array($operate[$key[$i]]) && ($i != count($key) - 1)) {
+            $operate = &$operate[$key[$i]];
+        } else if (!isset($operate[$key[$i]]) && ($i != count($key) - 1)) {
+            $operate[$key[$i]] = array();
+            $operate = &$operate[$key[$i]];
+        } else {
+            $operate[$key[$i]] = $value;
+        }
+    }
+
+    return json_encode($obj);
+}
+
+/** 对json序列化的字符串进行删除
+ * @param $key
+ * @param $obj
+ * @return string
+ */
+function jsonString_del($key, $obj)
+{
+    $obj = json_decode($obj, true);
+    $operate = &$obj;
+    $key = explode('.', $key);
+
+    for ($i = 0; $i < count($key); $i++) {
+        if (isset($operate[$key[$i]]) && is_array($operate[$key[$i]]) && ($i != count($key) - 1)) {
+            $operate = &$operate[$key[$i]];
+        } else if (!isset($operate[$key[$i]]) && ($i != count($key) - 1)) {
+            simpleError('jsonString_del() param is abnormal，please check "$key" param !!!',__FILE__,__LINE__);
+        } else {
+            unset($operate[$key[$i]]);
+        }
+    }
+
+    return json_encode($obj);
 }
