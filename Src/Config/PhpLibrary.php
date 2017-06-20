80062c7a9241444b28ce5e1bf723282a6b23766d
chenjiawen <604989282@qq.com>
2017-06-20 17:07:31 +0800

    mr.wen 手机发送短信验证码

--- Src/Config/PhpLibrary.php
+++ Src/Config/PhpLibrary.php
@@ -5,13 +5,26 @@
 
 return [
 
-    // curl类的配置参数
+    // 1. curl类的配置参数
     'curl_request' => [
         // curl对象的cookie存放文件目录，默认:/tmp/myCurlCookie
         'cookie_dir' => ''
     ],
 
-    // 图形验证码 (在session中存储的名称默认为：image_code)
+
+    // 2. 图形验证码 (在session中存储的名称默认为：image_code)
     'image_code' => '',
-    
+
+
+    // 3. 手机验证码(使用阿里大于)
+    'tel_code' => [
+        // a. 阿里大于模板变量占位符，默认为code
+        'template_variable' => '',
+
+        // b. 存储到redis中的key值（string类型）,默认为tel:code:[手机号]
+        'redis_key' => '',
+
+        // c. 有效时长，默认为10分钟
+        'redis_time' => '',
+    ]
 ];
--- Src/Tool/CommonTool.php
+++ Src/Tool/CommonTool.php
@@ -2,8 +2,10 @@
 
 namespace CjwLibrary\Src\Tool;
 
+use CjwLibrary\Src\Service\Alidayu\Alidayu;
 use Gregwar\Captcha\CaptchaBuilder;
 use Gregwar\Captcha\PhraseBuilder;
+use Illuminate\Support\Facades\Redis;
 use Ramsey\Uuid\Uuid;
 
 /** 通用的工具类
@@ -65,7 +67,62 @@ class CommonTool
         return $identifyingCode == $sysIdentifying ? true : false;
     }
 
+    /** 手机验证码的生成器
+     * @param $telephone
+     * @param string $SMSConfig
+     * @return array
+     */
+    public function telephoneCodeGenerator($telephone, $SMSConfig = 'default')
+    {
+        // 1. 准备参数
+        $code = $number = rand(100000, 999999);
 
+        $templateVariable = config('phpLibrary.tel_code.template_variable');
+        $templateVariable = empty($templateVariable) ? 'code' : $templateVariable;
+
+        $redisKey = config('phpLibrary.tel_code.redis_key');
+        $redisKey = empty($redisKey) ? 'tel:code:' : $redisKey;
+
+        $redisTime = config('phpLibrary.tel_code.redis_time');
+        $redisTime = empty($redisTime) ? 600 : $redisTime;
+
+        // 2. 发送短信
+        $alidayu = new Alidayu($SMSConfig);
+        $result = $alidayu->sendMessage($telephone, [$templateVariable => $code]);
+
+        if (!empty($result->result) &&
+            !empty($result->result->success) &&
+            $result->result->success == true
+        ) {
+            // 3. 存入缓存
+            Redis::setEx($redisKey . $telephone, $redisTime, $code);
+
+            return statusMsg(200, '短信发送成功', $result);
+        } else {
+            return statusMsg(500, '短信发送失败', $result);
+        }
+    }
+
+    /** 手机验证码的验证器(验证一次则被清空)
+     * @param $telephone
+     * @param $code
+     * @return array
+     */
+    public function telephoneCodeVerification($telephone, $code)
+    {
+        //1. 准备参数
+        $redisKey = config('phpLibrary.tel_code.redis_key');
+        $redisKey = empty($redisKey) ? 'tel:code:' : $redisKey;
+
+        // 2. 获取redis中数据
+        $result = Redis::get($redisKey . $telephone);
+
+        if (empty($result) || $result != $code) {
+            return statusMsg(400, '手机验证码验证失败', ['redis_value' => $result, 'code' => $code]);
+        }
+        Redis::del($redisKey . $telephone);
+        return statusMsg(200, '手机验证码验证成功', ['redis_value' => $result]);
+    }
 
     /** 生成uuid
      * @return mixed
